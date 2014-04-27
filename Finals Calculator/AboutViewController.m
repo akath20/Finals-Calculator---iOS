8f6e100bf0ba51015ff136860d01d2e2099b3863
snipper9 <akath20@charter.net>
2014-04-27 19:51:51 -0400

    Update

--- Finals Calculator/AboutViewController.m
+++ Finals Calculator/AboutViewController.m
@@ -97,6 +97,8 @@
     
     NSString *emailBody = [NSString stringWithFormat:@"\n\r\n\r\n\riOS Version: %@\n\rDevice: %@\n\rApp Version: %@\n\rApp Name: %@", [[UIDevice currentDevice] systemVersion], deviceType, [[NSBundle mainBundle] objectForInfoDictionaryKey: @"CFBundleShortVersionString"], [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"]];
     [emailSheet setMessageBody:emailBody isHTML:NO];
+    [emailSheet setToRecipients:[NSArray arrayWithObject:@"akath20@gmail.com"]];
+
     
     // Present the mail composition interface.
     [self presentViewController:emailSheet animated:true completion:nil];
